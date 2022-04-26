e208a2e9e124fe63127d53bdf865f7eb4cdb4de0
teresa pelinski <teresapelinski@gmail.com>
2022-04-26 10:36:57 +0000

    cleaned render, added .clang-format

--- render.cpp
+++ render.cpp
@@ -7,123 +7,91 @@
 
 http://bela.io
 
+Using:
 C++ Real-Time Audio Programming with Bela - Lecture 11: Circular buffers
 circular-buffer: template code for implementing delays
++ Code from the delay Bela Example
++ Audio Effects book
 */
 
 #include <Bela.h>
 #include <libraries/Gui/Gui.h>
 #include <libraries/GuiController/GuiController.h>
 #include <vector>
-#include "lib/MonoFilePlayer.h"
 
-// Name of the sound file (in project folder)
-std::string gFilename = "sample.wav";
+// Delay Buffer
+unsigned int gDelayBufferSize; // in samples
+unsigned int gNumChannels = 2;
+float gMaxDelayTime = 0.5; // in seconds
+std::vector<float> gDelayBuffer[2];
 
-// Object that handles playing sound from a buffer
-MonoFilePlayer gPlayer;
+// Pointers for the circular Buffer
+unsigned int gWritePointer = 0;
+unsigned int gReadPointer = 0;
 
 // Browser-based GUI to adjust parameters
 Gui gGui;
 GuiController gGuiController;
-// add sketch js
 
-// TODO: declare variables for circular buffer
-unsigned int gWritePointer = 0;
-unsigned int gReadPointer = 0;
+// Parameters
+float gDelayAmount = 1.0;     // Amount of delay
+float gFB = 0.999;            // gDelayFeedbackAmount // Amount of feedback
+float gDelayAmountPre = 0.75; // Level of pre-delay input
+float gDryMix = 0.2;
 
-// assuming number of channels is 2
-unsigned int gNumChannels = 2;
-std::vector<float> gDelayBuffer[2]; 
+bool setup(BelaContext *context, void *userData) {
+  // guiController Initialise
+  gGui.setup(context->projectName);
+  gGuiController.addSlider("Delay in s", 0.1, 0, gMaxDelayTime, 0);
 
+  // Allocate memory for the circular buffer (0.5s)
+  gDelayBufferSize = gMaxDelayTime * context->audioSampleRate; // Initialised here since it depends on the sample rate
+  for (unsigned int i = 0; i < gNumChannels; i++) {
+    gDelayBuffer[i].resize(gDelayBufferSize);
+  }
 
-// unsigned int gOffset = 0;
-
-// Amount of delay
-float gDelayAmount = 1.0;
-// Amount of feedback
-float gFB = 0.999; //gDelayFeedbackAmount
-// Level of pre-delay input
-float gDelayAmountPre = 0.75;
-float gDryMix =0.2;
-
-bool setup(BelaContext *context, void *userData)
-{
-	// Load the audio file
-	if (!gPlayer.setup(gFilename))
-	{
-		rt_printf("Error loading audio file '%s'\n", gFilename.c_str());
-		return false;
-	}
-
-	// Print some useful info
-	rt_printf("Loaded the audio file '%s' with %d frames (%.1f seconds)\n",
-			  gFilename.c_str(), gPlayer.size(),
-			  gPlayer.size() / context->audioSampleRate);
-
-	// guiController
-	gGui.setup(context->projectName);
-	gGuiController.addSlider("Delay in s", 0.1, 0, 0.49, 0);
-
-	// Allocate memory for the circular buffer (0.5s)
-	for (unsigned int i=0; i<gNumChannels; i++){
-		gDelayBuffer[i].resize(0.5 * context->audioSampleRate);
-	}
-
-	return true;
+  return true;
 }
 
-void render(BelaContext *context, void *userData)
-{
-	unsigned int delayBufferSize = gDelayBuffer[0].size();
+void render(BelaContext *context, void *userData) {
+  // now delay is controlled in gui, read at every render iteration
+  float delayInS = gGuiController.getSliderValue(0);
+  int delayInSamples = delayInS * context->audioSampleRate;
+  gReadPointer = (gWritePointer - delayInSamples + gDelayBufferSize) % gDelayBufferSize;
 
-	// now delay is controlled in gui, read at every render iteration
-	float delayInS = gGuiController.getSliderValue(0);
-	int delayInSamples = delayInS * context->audioSampleRate;
-	// gOffset = delayInS * context->audioSampleRate; // crop to int?
-	gReadPointer = (gWritePointer - delayInSamples + delayBufferSize) % delayBufferSize;
+  for (unsigned int n = 0; n < context->audioFrames; n++) {
+    float in[gNumChannels];
+    float out[gNumChannels];
 
+    //  Now we can write it into the delay buffer
+    for (unsigned int i = 0; i < gNumChannels; i++) {
+      in[i] = audioRead(context, n, i);
 
-	for (unsigned int n = 0; n < context->audioFrames; n++)
-	{
-		float in[gNumChannels];
-		float out[gNumChannels];
+      // Calculate the sample that will be written into the delay buffer...
+      // 1. Multiply the current (dry) sample by the pre-delay gain level (set
+      // above)
+      // 2. Get the previously delayed sample from the buffer, multiply it by
+      // the feedback gain and add it to the current sample
+      gDelayBuffer[i][gWritePointer] = gDelayAmountPre * in[i] + gDelayBuffer[i][gReadPointer] * gFB;
+      // Get the delayed sample (by reading `gDelayInSamples` many samples
+      // behind our current write pointer) and add it to our output sample
+      out[i] = gDryMix * in[i] + gDelayBuffer[i][gReadPointer] * gDelayAmount;
+      // Write to output channels
+      audioWrite(context, n, 0, out[i]);
+    }
 
-        // Calculate the sample that will be written into the delay buffer...
-        // 1. Multiply the current (dry) sample by the pre-delay gain level (set above)
-        // 2. Get the previously delayed sample from the buffer, multiply it by the feedback gain and add it to the current sample
-
-
-        //  Now we can write it into the delay buffer
-		for (unsigned int i=0; i<gNumChannels; i++){
-			in[i] = audioRead(context,n,i);
-
-			gDelayBuffer[i][gWritePointer] = gDelayAmountPre * in[i] + gDelayBuffer[i][gReadPointer] * gFB;
-			// Get the delayed sample (by reading `gDelayInSamples` many samples behind our current write pointer) and add it to our output sample
-			out[i] = gDryMix*in[i] + gDelayBuffer[i][gReadPointer] * gDelayAmount;
-			// Write the input and output to different channels
-			audioWrite(context, n, 0, out[i]);
-		}
-
-		gWritePointer++;
-		if (gWritePointer >= delayBufferSize)
-		{
-			gWritePointer = 0;
-		}
-		gReadPointer++;
-		if (gReadPointer >= delayBufferSize)
-		{
-			gReadPointer = 0;
-		}
-	}
+    // Increase pointer value. If the pointer is at the end of the buffer, wrap
+    // it to 0 (circular buffer)
+    gWritePointer = (gWritePointer + 1) % gDelayBufferSize;
+    gReadPointer = (gReadPointer + 1) % gDelayBufferSize;
+  }
 }
 
-void cleanup(BelaContext *context, void *userData)
-{
-}
+void cleanup(BelaContext *context, void *userData) {}
 
 // TODO
-// _ github
-// _ file reading and store to file
+// x github
+// o file reading and store to file : not necessary since the project is rt
 // _ filter parameters
 // _ interpolation
+// _ doppler effect
