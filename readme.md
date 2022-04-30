aa57f5737fd3426dec13b68c931836cd6f34bc8b
teresa pelinski <teresapelinski@gmail.com>
2022-04-30 12:47:26 +0000

    moved delay code to process_delay function, added threading for beattracking

---
+++ readme.md
@@ -0,0 +1,5 @@
+download and compile aubio
+````
+git clone https://github.com/aubio/aubio
+make
+````
--- render.cpp
+++ render.cpp
@@ -16,20 +16,23 @@ circular-buffer: template code for implementing delays
 
 #include <Bela.h>
 #include <cmath> // floorf, fmodf
+#include <libraries/Fft/Fft.h>
 #include <libraries/Gui/Gui.h>
 #include <libraries/GuiController/GuiController.h>
 #include <libraries/math_neon/math_neon.h>
 #include <vector>
 
+#include <aubio.h>
+
 // Delay Buffer
 unsigned int gDelayBufferSize; // in samples
-unsigned int gNumChannels = 2;
+const unsigned int gNumChannels = 2;
 float gMaxDelayTime = 0.5; // in seconds
 std::vector<float> gDelayBuffer[2];
 
 // Pointers for the circular Buffer
-unsigned int gWritePointer = 0; // stores the current signal (to be read later)
-float gReadPointer = 0;		// float since it will be used for interpolation. read pointer retrieves the delayed signal
+unsigned int gDelayWritePointer = 0; // stores the current signal (to be read later)
+float gDelayReadPointer = 0;	     // float since it will be used for interpolation. read pointer retrieves the delayed signal
 
 // Browser-based GUI to adjust parameters (parameters controlled from GUI start by p)
 Gui gGui;
@@ -42,15 +45,34 @@ float pDelayFeedbackLevel; // Level of feedback
 // float pDelayLevelPre;	    // Level of pre-delay input (removed because it doesn't do much)
 float pVibratoLFOFrequency; // LFO vibrato frequency
 
-// Doppler effect parameters
-// float gDelta; // Growth parameter, derivative of the time-varying delay
-// float pSoundSpeed;
-// float pSourceSpeed;   // Velocity with which the source moves towards the listener
-// float pListenerSpeed; // Velocity with which the listener moves towards the source
-
 // Helpers
-float gPhase;
+float gPhase = 0;
 float gInverseSampleRate;
+int gAudioSampleRate; // save context->audioSampleRate here to pass it to the aubio thread
+
+// FFT / Beat tracking
+std::vector<float> gBeatTrackerBuffer;
+unsigned int gBeatTrackerBufferSize = 2048;
+
+// const int gFftSize = 1024; // FFT window size in samples
+// int gHopSize = 256;	   // How often we calculate a window
+// int gHopCounter = 0;
+// // Circular buffer and pointer for assembling a window of samples
+// const int gFFTBufferSize = 16384;
+// int gFFTInputBufferPointer = 0;
+// // Circular buffer for collecting the output of the overlap-add process
+// int gFFTOutputBufferWritePointer = gFftSize + gHopSize;
+// int gFFTOutputBufferReadPointer = 0;
+// // Buffer to hold the windows for FFT analysis and synthesis
+// std::vector<float> gAnalysisWindowBuffer;
+// std::vector<float> gSynthesisWindowBuffer;
+// Thread for FFT processing
+AuxiliaryTask gBeatTrackingTask;
+int gCachedInputBufferPointer = 0;
+
+std::vector<float> process_delay(std::vector<float> in);
+void process_bt_background(void *);
+void process_bt(std::vector<float> const &inBuffer, unsigned int inPointer);
 
 bool setup(BelaContext *context, void *userData) {
 
@@ -60,20 +82,28 @@ bool setup(BelaContext *context, void *userData) {
   gGuiController.addSlider("Delay in s", 0.1, 0, gMaxDelayTime, 0);
   gGuiController.addSlider("Dry mix", 0.75, 0, 1, 0);
   gGuiController.addSlider("Feedback Level", 0.2, 0, 1, 0);
-  // gGuiController.addSlider("Pre-delay level", 0.75, 0, 1, 0);
   gGuiController.addSlider("Vibrato LFO frequency in Hz", 10, 0, 20, 0);
-  // gGuiController.addSlider("Sound speed in m/s", 340.0, 0, 500, 0);
-  // gGuiController.addSlider("Source speed in m/s", 20, 0, 200, 0);
-  // gGuiController.addSlider("Listener speed in m/s", 10, 0, 200, 0);
 
-  // Precalc inverse sample rate
-  gInverseSampleRate = 1.0 / context->audioSampleRate;
+  gInverseSampleRate = 1.0 / context->audioSampleRate; // Precalc inverse sample rate
+  gAudioSampleRate = context->audioSampleRate;	       // ave audio sample rate to global varible so that it can be used in auxiliary task
 
-  // Allocate memory for the circular buffer (0.5s)
+  // Allocate memory for the circular buffers
   gDelayBufferSize = gMaxDelayTime * context->audioSampleRate; // Initialised here since it depends on the sample rate
   for (unsigned int i = 0; i < gNumChannels; i++) {
-    gDelayBuffer[i].resize(gDelayBufferSize);
+    gDelayBuffer[i].resize(gDelayBufferSize); // two channels so that it can be scaled to eg ping pong delays
   }
+  gBeatTrackerBuffer.resize(gBeatTrackerBufferSize);
+
+  // Calculate the window
+  // gAnalysisWindowBuffer.resize(gFftSize);
+  // for (int n = 0; n < gFftSize; n++) {
+  //   // Hann window
+  //   gAnalysisWindowBuffer[n] = 0.5f * (1.0f - cosf(2.0 * M_PI * n / (float)(gFftSize - 1)));
+  // }
+  // recalculate_window(gFftSize);
+
+  // Set up the thread for the beat tracker
+  gBeatTrackingTask = Bela_createAuxiliaryTask(process_bt_background, 50, "bela-process-bt");
 
   return true;
 }
@@ -83,83 +113,169 @@ void render(BelaContext *context, void *userData) {
   // Delay parameters from GUI
   pDelayTime = gGuiController.getSliderValue(0); // This is also sweepwidth
   pDelayDryMix = gGuiController.getSliderValue(1);
-  pDelayFeedbackLevel = gGuiController.getSliderValue(2); // Level of feedback
-  // pDelayLevelPre = gGuiController.getSliderValue(3);	   // Level of pre-delay input
+  pDelayFeedbackLevel = gGuiController.getSliderValue(2);  // Level of feedback
   pVibratoLFOFrequency = gGuiController.getSliderValue(3); // LFO vibrato frequency
-  // pSoundSpeed = gGuiController.getSliderValue(4);
-  // pSourceSpeed = gGuiController.getSliderValue(5);
-  // pListenerSpeed = gGuiController.getSliderValue(6);
-
-  // Calculate the delay growth rate (derivative of the time-varying delay)
-  // gDelta = -(pListenerSpeed + pSourceSpeed) / (pSourceSpeed - pSourceSpeed);
 
   // Fractionary Delay in samples. Subtract 3 samples to the delay pointer to make sure we have enough previous sampels to interpolate with
   float delayInSamples =
       (pDelayTime * context->audioSampleRate) * (0.5f + 0.1f * sinf_neon(gPhase * 2.0 * M_PI)); // scaled so that vibrato (sinf) is less intense
-  gReadPointer = fmodf(((float)gWritePointer - (float)delayInSamples + (float)gDelayBufferSize - 3.0),
-		       (float)gDelayBufferSize); // mod operator % only defined for integeers, so I use fmodf instead
+  gDelayReadPointer = fmodf(((float)gDelayWritePointer - (float)delayInSamples + (float)gDelayBufferSize - 3.0),
+			    (float)gDelayBufferSize); // mod operator % only defined for integeers, so I use fmodf instead
 
-  // ?? why allocate memory here instead of in setup???
-  float in[gNumChannels];
-  float out[gNumChannels];
-  float interpolatedReadSample = 0.0;
+  // (in --[delay with vibrato and feedback]--> outDelay --[tremolo]--> out)
+  std::vector<float> in(2); // input signal 
+  std::vector<float> outDelay(2); // input signal after delay in 
+  std::vector<float> out(2); // final signal after delay and tremolo
+
 
   for (unsigned int n = 0; n < context->audioFrames; n++) {
+    // 1. Read input audio sample
+    for (unsigned int i = 0; i < gNumChannels; i++) {
+      in[i] = audioRead(context, n, i);
+    }
+    outDelay = process_delay(in);
+
+    // // 2.  Write sample that gets stored in the delay buffer: input sample (current input sample, in[i]) and the current delayed sample
+    // // (interpolatedReadSample) multiplied by the feedback level
+    // // if the feedback level is 0, that is, if the written sample into the buffer is equal to in, the delay line produces a single echo
+    // // (pDelayFeedbackLevel).
+    // // ???? IS THIS FLANGER???
+    // gDelayBuffer[i][gDelayWritePointer] = in[i] + pDelayFeedbackLevel * interpolatedReadSample;
+
+    // // 3. Read delayed sample from the delay buffer
+    // // Lnear interpolation in case we have a varying delay over time (e.g. vibrato/chorus/flanger)
+    // // Use linear interpolation to read a fractional index into the buffer. (necessary in case the delay is very small and for vibratto) Find the
+    // // fraction by which the read pointer sits between two samples and use this to adjust weights of the samples
+    // float fraction = gDelayReadPointer - floorf(gDelayReadPointer);
+    // int previousSample = (int)floorf(gDelayReadPointer);
+    // int nextSample = (previousSample + 1) % gDelayBufferSize;
+    // interpolatedReadSample = fraction * gDelayBuffer[i][nextSample] + (1.0 - fraction) * gDelayBuffer[i][previousSample]; // delayed sample
+
+    // // 4. Mix input signal with output signal
+    // // The output is the input plus the contents of the delay buffer (read with the readcursor) and weighted by the mix levels
+    // outDelay[i] = pDelayDryMix * in[i] + (1 - pDelayDryMix) * interpolatedReadSample;
+    // // test bypass
+    // // outDelay[i] = in[i];
+
+    // Tremolo block
+    // Beat tracking is only done in one channel!
+    // pass buffer
+
+    // // TODO beat tracking gives back a signal that changes the frequency of the LFO?
+
+    // // Store the sample ("in") in a buffer for the FFT
+    // // Increment the pointer and when full window has been
+    // // assembled, call process_bt()
+    // gBeatTrackerBuffer[gFFTInputBufferPointer] = outDelay[i];
+
+    // // Get the output sample from the output buffer
+    // float out = gFFTOutputBuffer[i][gFFTOutputBufferReadPointer];
+
+    // // Then clear the output sample in the buffer so it is ready for the next overlap-add
+    // gFFTOutputBuffer[i][gFFTOutputBufferReadPointer] = 0;
+
+    // // Scale the output down by the overlap factor (e.g. how many windows overlap per sample?)
+    // out *= (float)gHopSize / (float)gFftSize;
+
+    // // Increment the hop counter and start a new FFT if we've reached the hop size
+    // // ????? this happens twice with the channels?
+    // if (gHopCounter + 1 >= gHopSize) {
+    //   gCachedInputBufferPointer = gFFTInputBufferPointer;
+    //   Bela_scheduleAuxiliaryTask(gBeatTrackingTask);
+    // }
 
     for (unsigned int i = 0; i < gNumChannels; i++) {
-
-      // 1. Read input audio sample
-      in[i] = audioRead(context, n, i);
-
-      // 2.  Write sample that gets stored in the delay buffer: input sample (current input sample, in[i]) and the current delayed sample
-      // (interpolatedReadSample) multiplied by the feedback level
-      // if the feedback level is 0, that is, if the written sample into the buffer is equal to in, the delay line produces a single echo
-      // (pDelayFeedbackLevel).
-      // ???? IS THIS FLANGER???
-      // gDelayBuffer[i][gWritePointer] = pDelayLevelPre * in[i] + pDelayFeedbackLevel * interpolatedReadSample;
-      gDelayBuffer[i][gWritePointer] = in[i] + pDelayFeedbackLevel * interpolatedReadSample;
-
-      // 3. Read delayed sample from the delay buffer
-      // Lnear interpolation in case we have a varying delay over time (e.g. vibrato/chorus/flanger)
-      // Use linear interpolation to read a fractional index into the buffer. (necessary in case the delay is very small and for vibratto) Find the
-      // fraction by which the read pointer sits between two samples and use this to adjust weights of the samples
-      float fraction = gReadPointer - floorf(gReadPointer);
-      int previousSample = (int)floorf(gReadPointer);
-      int nextSample = (previousSample + 1) % gDelayBufferSize;
-      interpolatedReadSample = fraction * gDelayBuffer[i][nextSample] + (1.0 - fraction) * gDelayBuffer[i][previousSample]; // delayed sample
-
-      // 4. Mix input signal with output signal
-      // The output is the input plus the contents of the delay buffer (read with the readcursor) and weighted by the mix levels
-      out[i] = pDelayDryMix * in[i] + (1 - pDelayDryMix) * interpolatedReadSample;
-      // test bypass
-      // out[i] = in[i];
-
       // 5. Write the output sample into the audio output
-      audioWrite(context, n, i, out[i]);
-    }
+      audioWrite(context, n, i, outDelay[i]);
+    };
 
-    // Increase pointer value. If the pointer is at the end of the buffer, wrap
-    // it to 0 (circular buffer)
-    gWritePointer = (gWritePointer + 1) % gDelayBufferSize; // is an integer so it will always wrap around strictly to 0
-							    // gReadPointer =
-    // fmodf(((float)gReadPointer + 1.0 - gDelta), (float)gDelayBufferSize); // read pointer is fractional so may wrap around a fractional value
-    gReadPointer = fmodf(((float)gReadPointer + 1.0), (float)gDelayBufferSize);
-
-    // Update the LFO phase, keeping it in the range 0-1
-    gPhase += pVibratoLFOFrequency * gInverseSampleRate;
-    if (gPhase > 1.0) {
-      gPhase -= 1.0;
-    }
+    // Increase and wrap pointers
+    // gHopCounter = (gHopCounter + 1) % gHopSize;
+    // gFFTOutputBufferReadPointer = (gFFTOutputBufferReadPointer + 1) % gFFTBufferSize;
+    // gFFTInputBufferPointer = (gFFTInputBufferPointer + 1) % gFFTBufferSize;
   }
 }
 
 void cleanup(BelaContext *context, void *userData) {}
 
-// TODO
-// x github
-// o file reading and store to file : not necessary since the project is rt
-// x filter parameters
-// x interpolation
-// x vibratto
-// _ doppler effect: not working
-// _ multiple sources where each source produces its own doppler effect
+std::vector<float> process_delay(std::vector<float> in) {
+  // receives the 'in' vector with the current (i.e. at the current timeframe in the render for loop) input samples (one per channel) and returns the
+  // samples processed with the delay (one per channel)
+  std::vector<float> out(2);
+  float interpolatedReadSample = 0.0;
+
+  for (unsigned int i = 0; i < in.size(); i++) {
+
+    // 1.  Write sample that gets stored in the delay buffer: input sample (current input sample, in[i]) and the current delayed sample
+    // (interpolatedReadSample) multiplied by the feedback level
+    // if the feedback level is 0, that is, if the written sample into the buffer is equal to in, the delay line produces a single echo
+    // (pDelayFeedbackLevel).
+    gDelayBuffer[i][gDelayWritePointer] = in[i] + pDelayFeedbackLevel * interpolatedReadSample;
+
+    // 2. Read delayed sample from the delay buffer
+    // Lnear interpolation in case we have a varying delay over time (e.g. vibrato/chorus/flanger)
+    // Use linear interpolation to read a fractional index into the buffer. (necessary in case the delay is very small and for vibratto) Find the
+    // fraction by which the read pointer sits between two samples and use this to adjust weights of the samples
+    float fraction = gDelayReadPointer - floorf(gDelayReadPointer);
+    int previousSample = (int)floorf(gDelayReadPointer);
+    int nextSample = (previousSample + 1) % gDelayBufferSize;
+    interpolatedReadSample = fraction * gDelayBuffer[i][nextSample] + (1.0 - fraction) * gDelayBuffer[i][previousSample]; // delayed sample
+
+    // 3. Mix input signal with output signal
+    // The output is the input plus the contents of the delay buffer (read with the readcursor) and weighted by the mix levels
+    out[i] = pDelayDryMix * in[i] + (1 - pDelayDryMix) * interpolatedReadSample;
+  }
+
+  // Increase and wrap pointers
+  gDelayWritePointer = (gDelayWritePointer + 1) % gDelayBufferSize; // is an integer so it will always wrap around strictly to 0
+  gDelayReadPointer = fmodf(((float)gDelayReadPointer + 1.0), (float)gDelayBufferSize);
+
+  // Update the vibrato LFO phase, keeping it in the range 0-1
+  gPhase += pVibratoLFOFrequency * gInverseSampleRate;
+  if (gPhase > 1.0) {
+    gPhase -= 1.0;
+  }
+
+  return out;
+}
+
+// This function handles the FFT processing in this example once the buffer has
+// been assembled.
+
+void process_bt(std::vector<float> const &inBuffer, unsigned int inPointer) {
+  // static std::vector<float> unwrappedBuffer(gFftSize); // Container to hold the unwrapped values
+
+  // // Copy buffer into FFT input
+  // for (int n = 0; n < gFftSize; n++) {
+  //   // Use modulo arithmetic to calculate the circular buffer index
+  //   int circularBufferIndex = (inPointer + n - gFftSize + gFFTBufferSize) % gFFTBufferSize;
+  //   unwrappedBuffer[n] = inBuffer[circularBufferIndex] * gAnalysisWindowBuffer[n];
+  // }
+
+  // // Process the FFT based on the time domain input
+  // gFft.fft(unwrappedBuffer);
+
+  // // Robotise the output
+  // for (int n = 0; n < gFftSize; n++) {
+  //   float amplitude = gFft.fda(n);
+  //   gFft.fdr(n) = amplitude;
+  //   gFft.fdi(n) = 0;
+  // }
+
+  // // Run the inverse FFT
+  // gFft.ifft();
+
+  // // Add timeDomainOut into the output buffer
+  // for (int n = 0; n < gFftSize; n++) {
+  //   int circularBufferIndex = (outPointer + n - gFftSize + gFFTBufferSize) % gFFTBufferSize;
+  //   outBuffer[circularBufferIndex] += gFft.td(n) * gSynthesisWindowBuffer[n];
+  // }
+}
+
+// This function runs in an auxiliary task on Bela, calling process_bt
+void process_bt_background(void *) {
+  process_bt(gBeatTrackerBuffer, gCachedInputBufferPointer);
+
+  // Update the output buffer write pointer to start at the next hop
+  // gFFTOutputBufferWritePointer = (gFFTOutputBufferWritePointer + gHopSize) % gFFTBufferSize;
+}
